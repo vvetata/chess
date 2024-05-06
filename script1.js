0a3f4dee7511c4e9618729eeff3be440a1fc4270
Liza Tarakanova <liztarakanova@mail.ru>
2024-05-07 00:17:28 +0500

    рабочие слайдеры

--- script1.js
+++ script1.js
@@ -1,29 +1,33 @@
 //слайдер grid
 
-let sutitle = document.querySelector('.transform__subtitle');
+let subtitle = document.querySelector('.transform__subtitle');
 let title = document.querySelector('.transform__title');
-let plane = document.querySelector('.transform__image');
 let container = document.querySelector('.transform__items');
-let cards = document.querySelectorAll('.transform__item');
-let item2 = cards[1];
-let port = document.querySelectorAll('.transform__item_cont');
-let port1 = port[0];
-let item5 = cards[4];
-let port4 = port[3];
+
+let content = document.querySelectorAll('.transform__content');
+let content2 = content[1];
+let slide = document.querySelectorAll('.transform__item');
+let slide1 = slide[0];
+let content5 = content[4];
+let slide4 = slide[3];
 
 
 //адаптация карточек
 if(window.innerWidth <= 1230) {
-    port1.append(item2);
-    port4.append(item5);
-    port[4].remove();
-    port[1].remove();
+    slide1.append(content2);
+    slide4.append(content5);
+    slide[4].remove();
+    slide[1].remove();
 }
 
 //адаптация хэдэра
 if(window.innerWidth < 734) {
-    title.append(sutitle);
+    title.append(subtitle);
 }
+
+//адаптация padding у transform__content
+content[1].style.paddingTop = '28px';
+content[4].style.paddingTop = '28px';
 
 // для слайдера
 
@@ -35,22 +39,47 @@ let indicators = document.querySelectorAll('.nav-transform__indicator');
 let indIndex = 0;
 let position = 0;
 
-/*if(window.innerWidth < 1230) {
+if(window.innerWidth < 1230) {
 
 
 
 btnNext.onclick = () => {
-    itemsGrid.scrollLeft += itemGrid.clientWidth + 60;
+    itemsGrid.scrollLeft += itemGrid.clientWidth + 21;
+    if (indIndex >= indicators.length-1) {
+        indIndex = indIndex;
+    } else {
+        indIndex++;
+    }
     thisSlide(indIndex);
-    indIndex++;
+    isNotActive();
 }
 btnPrew.onclick = () => {
-    itemsGrid.scrollLeft -= itemGrid.clientWidth + 60;
+    itemsGrid.scrollLeft -= itemGrid.clientWidth + 20;
+    if (indIndex < 1) {
+        return;        
+    } 
+    else{
+        indIndex--;
+    }
     thisSlide(indIndex);
-    indIndex--;
+    isNotActive();
 }
 
-}
+
+
+// индикаторы
+
+/*РАБОТАЕТ */
+indicators.forEach((ind, index) => {
+    ind.addEventListener('click', () => {
+        itemsGrid.scrollLeft = (itemGrid.clientWidth + 21) * index;
+        indIndex = index;
+        thisSlide(indIndex);
+        isNotActive();
+    })
+    })
+
+
 
 let thisSlide = (index) => {
     for (let ind of indicators) {
@@ -59,74 +88,24 @@ let thisSlide = (index) => {
     indicators[index].classList.add('nav-transform__indicators_active');
 }
 
-// индикаторы
+let isNotActive = () => {
+if (indicators[0].classList.contains('nav-transform__indicators_active')) {
+    btnPrew.style.backgroundColor = '#D9D9D9';
+} else if (indicators[indicators.length - 1].classList.contains('nav-transform__indicators_active')) {
+    btnNext.style.backgroundColor = '#D9D9D9';
+} else {
+    btnPrew.style.backgroundColor = '#313131';
+    btnNext.style.backgroundColor = '#313131';
+}
 
-
-
-indicators.forEach((ind, index) => {
-    ind.addEventListener('click', () => {
-        itemsGrid.scrollLeft = (itemGrid.clientWidth + 60) * index;
-        indIndex = index;
-        thisSlide(indIndex);
-    })
-
-})
-
+}
+}
 
 
 /*
-let indicator1 = document.querySelector('.nav-transform__indicator_1');
-let indicator2 = document.querySelector('.nav-transform__indicator_2');
-let indicator3 = document.querySelector('.nav-transform__indicator_3');
-let indicator4 = document.querySelector('.nav-transform__indicator_4');
-let indicator5 = document.querySelector('.nav-transform__indicator_5');
-let svg1 = indicator1.firstElementChild;
-
-indicator1.onclick = () => {
-    itemsGrid.scrollLeft = 0;
-    indicator1.style.color = '#313131';
-    indicator2.style.color = '#D9D9D9';
-    indicator3.style.color = '#D9D9D9';
-    indicator4.style.color = '#D9D9D9';
-    indicator5.style.color = '#D9D9D9';
-}
-indicator2.onclick = () => {
-    itemsGrid.scrollLeft = itemGrid.clientWidth + 60;
-    indicator2.style.color = '#313131';
-    indicator1.style.color = '#D9D9D9';
-    indicator3.style.color = '#D9D9D9';
-    indicator4.style.color = '#D9D9D9';
-    indicator5.style.color = '#D9D9D9';
-}
-indicator3.onclick = () => {
-    itemsGrid.scrollLeft = (itemGrid.clientWidth + 60) * 2;
-    indicator3.style.color = '#313131';
-    indicator2.style.color = '#D9D9D9';
-    indicator1.style.color = '#D9D9D9';
-    indicator4.style.color = '#D9D9D9';
-    indicator5.style.color = '#D9D9D9';
-}
-indicator4.onclick = () => {
-    itemsGrid.scrollLeft = (itemGrid.clientWidth + 60) * 3;
-    indicator4.style.color = '#313131';
-    indicator2.style.color = '#D9D9D9';
-    indicator3.style.color = '#D9D9D9';
-    indicator1.style.color = '#D9D9D9';
-    indicator5.style.color = '#D9D9D9';
-}
-indicator5.onclick = () => {
-    itemsGrid.scrollLeft = (itemGrid.clientWidth + 60) * 4;
-    indicator5.style.color = '#313131';
-    indicator2.style.color = '#D9D9D9';
-    indicator3.style.color = '#D9D9D9';
-    indicator4.style.color = '#D9D9D9';
-    indicator1.style.color = '#D9D9D9';
-}
-*/
-
 const nextSlide = () => {
     position = 537;
     itemsGrid.style.left = -position + 'px';
 }
 
-btnNext.addEventListener('click', nextSlide);
+btnNext.addEventListener('click', nextSlide);*/
